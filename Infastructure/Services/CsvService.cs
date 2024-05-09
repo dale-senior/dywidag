a94ca0a72ce9cb1ac23db66baac7820a22da5b63
dale-senior <dale_senior@hotmail.co.uk>
2024-05-09 15:25:19 +0100

    Update CsvService.cs
    added csv writer that takes a dictionary of key value pairs along with headers to generate a csv file

--- Infastructure/Services/CsvService.cs
+++ Infastructure/Services/CsvService.cs
@@ -7,19 +7,25 @@ namespace dywidag.Infastructure.Services
 {
     public class CsvService : ICsvService
     {
-        public CsvService()
-        {
-
-        }
-
         public bool OutputToCsvFile(Dictionary<int, bool> output, string[] headers)
         {
-            using (StreamWriter writer = new StreamWriter("LeapYears.csv"))
+            try
             {
-                
-            }
+                using (StreamWriter writer = new StreamWriter("LeapYears.csv"))
+                {
+                    writer.WriteLine(string.Join(",", headers));
+                    foreach (var keyValuePair in output)
+                    {
+                        writer.WriteLine($"{keyValuePair.Key},{keyValuePair.Value}");
+                    }
+                }
 
-            return true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
